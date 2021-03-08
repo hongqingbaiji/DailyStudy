2ec1538bc00b6ccab2bcd22ccc2c75a0c53c02af
hongqingbaiji <869227436@qq.com>
2021-03-08 23:29:06 +0800

    0308

--- 20210308/todolist.js
+++ 20210308/todolist.js
@@ -4,12 +4,11 @@ $(function() {
     $('#title').on('keydown', function(e) {
         if (e.keyCode === 13) {
             if ($(this).val() == '') {
+                alert('请输入内容');
                 return false;
             } else {
-
                 // 先读取本地存储原来的数据  封装一个函数
                 var local = getDate();
-                console.log((local));
                 //把local数组进行更新数据把最新的数据追加给local数组
                 local.push({ title: $(this).val(), done: false });
                 //把这个数组local 存储到本地存储 也是封装一个函数，并把local传递到函数中
@@ -18,10 +17,26 @@ $(function() {
                 load();
                 $(this).val('');
             }
-
-
         }
-    })
+    });
+    //3.删除操作
+    $('ol, ul').on('click', 'a', function() {
+        //先获取本地存储，在修改数据，在保存到本地存储，然后重新渲染
+        var data = getDate();
+        var index = $(this).attr('id');
+        data.splice(index, 1);
+        saveDate(data);
+        load();
+    });
+    //4.点击复选框切换正在进行和已完成操作；
+    $('ol, ul').on('click', 'input', function() {
+        //先获取本地数据，修改数据，保存到本地存储，重新渲染页面
+        var data = getDate();
+        var index = $(this).siblings('a').attr('id');
+        data[index].done = $(this).prop('checked');
+        saveDate(data);
+        load();
+    });
 
     //读取本地存储的数据
     function getDate() {
@@ -32,11 +47,11 @@ $(function() {
         } else {
             return [];
         }
-    }
+    };
     //保存本地存储数据
     function saveDate(data) {
         localStorage.setItem('todolist', JSON.stringify(data));
-    }
+    };
     //渲染加载页面
     function load() {
         //读取本地数据
@@ -44,9 +59,15 @@ $(function() {
         //遍历这个数据
         //遍历数据之前要先把ol里面的数据清空
         $('ol').empty();
+        $('ul').empty();
         $.each(data, function(i, n) {
+            $('ol').prepend('<li><input type = "checkbox"><p>' + n.title + '</p><a href = "javascript:;" id="' + i + '">x</a></li>');
+            // if (n.done) {
+            //     $('ul').prepend('<li><input type = "checkbox" checked="checked"><p>' + n.title + '</p><a href = "javascript:;" id="' + i + '">x</a></li>');
+            // } else {
+            //     $('ol').prepend('<li><input type = "checkbox"><p>' + n.title + '</p><a href = "javascript:;" id="' + i + '">x</a></li>');
+            // }
 
-            $('ol').prepend('<li><input type = "checkbox"><p>' + n.title + '</p><a href = "javascript:;">删除</a></li>');
         });
     }
 
