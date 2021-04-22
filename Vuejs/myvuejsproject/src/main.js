e14df77c1de3807664fab84be387b02ccdf5a933
hongqingbaiji <869227436@qq.com>
2021-04-23 00:18:49 +0800

    vuecli3

--- Vuejs/myvuejsproject/src/main.js
+++ Vuejs/myvuejsproject/src/main.js
@@ -1,19 +1,18 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import { config } from 'vue/types/umd';
+// import { config } from 'vue/types/umd';
 import App from './App'
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
+
 new Vue({
   el: '#app',
-  components: { App },
-  template: '<App/>'
+  // components: { App },
+  // template: '<App/>'
+  render: function(createElement) {
+    return createElement(App)
+  }
 })
-
-function sum(num1, num2) {
-  return num1 + num2;
-}
-console.log(sum(10, 20));
