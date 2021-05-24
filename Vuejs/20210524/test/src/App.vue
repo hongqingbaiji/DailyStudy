e870c9bd615922219f47caa2957718b9eb123d82
hongqingbaiji <869227436@qq.com>
2021-05-24 17:53:41 +0800

    0524

---
+++ Vuejs/20210524/test/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h3>标题</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {},
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
+
---
+++ Vuejs/20210524/test/src/router/index.js
@@ -0,0 +1,16 @@
+// 配置路由相关信息
+import VueRouter from 'vue-router'
+import Vue from 'vue'
+
+// 1. 通过Vue.use(插件),安装插件
+Vue.use(VueRouter)
+
+// 2. 创建VueRouter对象
+const routes = [
+
+]
+const router = VueRouter({
+  routes
+})
+
+export default router
