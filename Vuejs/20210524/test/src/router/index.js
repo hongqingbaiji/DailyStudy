c6f98d1686b5c36fb999bdbfed0527b0a632a1d9
hongqingbaiji <869227436@qq.com>
2021-05-24 23:51:30 +0800

    0524

--- Vuejs/20210524/test/src/router/index.js
+++ Vuejs/20210524/test/src/router/index.js
@@ -1,16 +1,18 @@
 // 配置路由相关信息
-import VueRouter from 'vue-router'
-import Vue from 'vue'
+import VueRouter from 'vue-router';
+import Vue from 'vue';
 
 // 1. 通过Vue.use(插件),安装插件
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 // 2. 创建VueRouter对象
 const routes = [
 
-]
-const router = VueRouter({
+];
+const router = new VueRouter({
+  //配置路由和组件之间的应用关系
   routes
-})
+});
 
-export default router
+// 3. 将router对象传入到vue实例中
+export default router;
