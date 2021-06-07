c8ca6bd20fcb97fb6e40643233078b061594122c
hongqingbaiji <869227436@qq.com>
2021-06-07 19:01:53 +0800

    0607

---
+++ Ajax/20210607/app.js
@@ -0,0 +1,25 @@
+// 引入express框架
+const express = require('express');
+// 路径处理模块
+const path = require('path');
+// 创建web服务器
+const app = express();
+
+// 静态资源访问服务功能
+app.use(express.static(path.join(__dirname, 'public')));
+
+// 创建路由
+app.get('/first', (req, res) => {
+  res.send('Hello Ajax');
+});
+
+app.get('/responseData', (req, res) => {
+  res.send({
+    "name": "wenhe"
+  });
+});
+
+// 监听端口
+app.listen(3000);
+// 控制台提示输出
+console.log('服务器启动成功');
