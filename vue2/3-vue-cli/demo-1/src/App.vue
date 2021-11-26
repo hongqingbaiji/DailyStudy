c894e45430c60f94654e3fddbbd16f1addb8b05c
hongqingbaiji <869227436@qq.com>
2021-11-26 18:57:59 +0800

    vue2

---
+++ vue2/3-vue-cli/demo-1/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>这是App组件</h1>
+    <!-- 步骤3：以标签形式使用注册好的组件 -->
+    <div>
+      
+    </div>
+    <Left></Left>
+    <Right></Right>
+  </div>
+</template>
+
+<script>
+// 步骤1：导入需要使用的 .vue 组件
+import Left from '@/components/Left';
+import Right from '@/components/Right';
+export default {
+  name: 'App',
+  components: {
+    // 步骤2：注册组件
+    Left,
+    Right
+  },
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
+
