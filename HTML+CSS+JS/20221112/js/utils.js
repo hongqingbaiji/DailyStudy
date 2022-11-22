99a02842b6d0d277b7db9627d2fc7c588314ce47
hongqingbaiji <869227436@qq.com>
2022-11-22 21:58:47 +0800

    1122

--- HTML+CSS+JS/20221112/js/utils.js
+++ HTML+CSS+JS/20221112/js/utils.js
@@ -488,38 +488,88 @@ var tools = {
 
   // 28. 封装AJAX
   xhr: (function () {
-    var o = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP')
-    if (!o) {
-      throw new Error('您的浏览器不支持异步发起HTTP请求')
-    }
     function _doAjax(opt) {
+      var o = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP')
+
+      if (!o) {
+        throw new Error('您的浏览器不支持异步发起HTTP请求')
+      }
+
       var opt = opt || {},
         type = (opt.type || 'GET').toUpperCase(),
-        async = opt.async || true,
+        async = '' + opt.async === 'false' ? false : true,
+        dataType = opt.dataType || 'JSON',
+        jsonp = opt.jsonp || 'cb',
+        jsonpCallback = opt.jsonpCallback || 'jQuery' + randomNum() + '_' + new Date().getTime(),
         url = opt.url,
         data = opt.data || null,
+        timeout = opt.timeout || 30000,
         error = opt.error || function () {},
         success = opt.success || function () {},
-        complete = opt.complete || function () {}
+        complete = opt.complete || function () {},
+        t = null
 
       if (!url) {
         throw new Error('您没有配置URL')
       }
+
+      if (dataType.toUpperCase() === 'JSONP' && type !== 'GET') {
+        throw new Error('如果dataType为JSONP,type请您设置GET或不设置')
+      }
+
+      if (dataType.toUpperCase() === 'JSONP') {
+        var oScript = document.createElement('script')
+        oScript.src =
+          url.indexOf('?') === -1
+            ? url + '?' + jsonp + '=' + jsonpCallback
+            : url + '&' + jsonp + '=' + jsonpCallback
+        document.body.appendChild(oScript)
+        document.body.removeChild(oScript)
+        window[jsonpCallback] = function (data) {
+          success(data)
+        }
+        return
+      }
+
+      o.onreadystatechange = function () {
+        if (o.readyState === 4) {
+          if ((o.status >= 200 && o.status < 300) || o.status === 304) {
+            switch (dataType.toUpperCase()) {
+              case 'JSON':
+                success(JSON.parse(o.responseText)) // 返回类型为JSON
+                break
+              case 'TEXT':
+                success(o.responseText) // 返回类型为TEXT
+                break
+              case 'XML':
+                success(o.responseXML)
+                break
+              default:
+                success(JSON.parse(o.responseText))
+            }
+          } else {
+            error()
+          }
+          complete()
+          clearTimeout(t)
+          t = null
+          o = null
+        }
+      }
+
       o.open(type, url, async)
       type === 'POST' && o.setRequestHeader('Content-type', 'application/x-www-form-urlencoded')
       o.send(type === 'GET' ? null : formatDatas(data))
 
-      o.onreadystatechange = function () {
-        if (o.readyState === 4 && o.status === 200) {
-          success(JSON.parse(o.responseText))
-        }
-
-        if (o.status === 404) {
-          error()
-        }
-        complete()
-      }
+      t = setTimeout(function () {
+        o.abort()
+        clearTimeout(t)
+        t = null
+        o = null
+        throw new Error('本次请求已超时,API地址:' + url)
+      }, timeout)
     }
+
     function formatDatas(obj) {
       var str = ''
       for (var key in obj) {
@@ -527,23 +577,38 @@ var tools = {
       }
       return str.replace(/&$/, '')
     }
+
+    function randomNum() {
+      var num = ''
+      for (var i = 0; i < 20; i++) {
+        num += Math.floor(Math.random() * 10)
+      }
+      return num
+    }
+
     return {
       ajax: function (opt) {
         _doAjax(opt)
       },
-      get: function (url, callback) {
+      get: function (url, dataType, successCB, errorCB, completeCB) {
         _doAjax({
           type: 'GET',
           url: url,
-          success: callback
+          dataType: dataType,
+          success: successCB,
+          error: errorCB,
+          complete: completeCB
         })
       },
-      post: function (url, data, callback) {
+      post: function (url, data, dataType, successCB, errorCB, completeCB) {
         _doAjax({
           type: 'POST',
           url: url,
           data: data,
-          success: callback
+          dataType: dataType,
+          success: successCB,
+          error: errorCB,
+          complete: completeCB
         })
       }
     }
