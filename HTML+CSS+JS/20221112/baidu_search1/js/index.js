41223180bce8978300619575f0bd932899299f4e
hongqingbaiji <869227436@qq.com>
2022-12-02 08:40:52 +0800

    1202

--- HTML+CSS+JS/20221112/baidu_search1/js/index.js
+++ HTML+CSS+JS/20221112/baidu_search1/js/index.js
@@ -1,61 +1,63 @@
 ;(function (doc) {
-  var searchInput = doc.getElementsByClassName('J_searchInput')[0],
-    wdList = doc.getElementsByClassName('J_wdList')[0],
-    listWrap = wdList.parentNode,
-    listTpl = doc.getElementById('J_listTpl').innerHTML
+  var oSearchInput = doc.getElementsByClassName('J_searchInput')[0],
+    oWdList = doc.getElementsByClassName('J_wdList')[0],
+    oListWrap = oWdList.parentNode,
+    oListTpl = doc.getElementById('J_listTpl').innerHTML
 
   var init = function () {
     bindEvent()
   }
 
   function bindEvent() {
-    searchInput.addEventListener('input', typeInput, false)
+    oSearchInput.addEventListener('input', typeInput, false)
   }
 
   function typeInput() {
     var val = _trimSpace(this.value),
       len = val.length
 
-    if (len > 0) {
-      getDatas(val, 'setDatas')
+    if (len) {
+      // getDatas(val, 'setDatas')
+      getDatas(val)
     } else {
-      wdList.innerHTML = ''
-      listWrap.style.display = 'none'
+      oWdList.innerHTML = ''
+      oListWrap.style.display = 'none'
     }
   }
 
   function renderList(data) {
-    var arr = data.s,
+    var list = '',
+      arr = data.s,
       len = arr.length,
-      list = '',
-      val = _trimSpace(searchInput.value)
+      val = _trimSpace(oSearchInput.value)
 
     if (len) {
-      arr.forEach(function (elem) {
-        list += listTpl.replace(/{{(.*?)}}/gim, function (node, key) {
-          return {
-            wdLink: elem,
-            wd: _setWdStyle(val, elem)
-          }[key]
-        })
+      arr.forEach(function (elem, idx) {
+        if (idx <= 3) {
+          list += oListTpl.replace(/{{(.*?)}}/g, function (node, key) {
+            return {
+              wdLink: elem,
+              wd: _setWdStyle(val, elem)
+            }[key]
+          })
+        }
       })
-      wdList.innerHTML = list
-      listWrap.style.display = 'block'
+      oWdList.innerHTML = list
+      oListWrap.style.display = 'block'
     } else {
-      wdList.innerHTML = ''
-      listWrap.style.display = 'none'
+      oWdList.innerHTML = ''
+      oListWrap.style.display = 'none'
     }
   }
 
   function getDatas(value, callbackName) {
-    //JSONP跨域
     // var oScript = doc.createElement('script')
     // oScript.src =
     //   'https://sp0.baidu.com/5a1Fazu8AA54nxGko9WTAnF6hhy/su?wd=' + value + '&cb=' + callbackName
+
     // doc.body.appendChild(oScript)
     // doc.body.removeChild(oScript)
 
-    // 使用封装好的JSONP跨域
     tools.xhr.ajax({
       url: 'https://sp0.baidu.com/5a1Fazu8AA54nxGko9WTAnF6hhy/su?wd=' + value,
       type: 'GET',
@@ -72,11 +74,11 @@
   // }
 
   function _trimSpace(str) {
-    return str.replace(/\s+/gim, '')
+    return str.replace(/\s+/g, '')
   }
 
   function _setWdStyle(value, word) {
-    return '<span class="font-normal">' + value + '</span>' + word.replace(value, '')
+    return '<span class="font-normal"> ' + value + '</span>' + word.replace(value, '')
   }
 
   init()
---
+++ HTML+CSS+JS/20221112/baidu_search1/js/index2.js
@@ -0,0 +1,86 @@
+;(function (doc) {
+  var searchInput = doc.getElementsByClassName('J_searchInput')[0],
+    wdList = doc.getElementsByClassName('J_wdList')[0],
+    listWrap = wdList.parentNode,
+    listTpl = doc.getElementById('J_listTpl').innerHTML
+
+  var init = function () {
+    bindEvent()
+  }
+
+  function bindEvent() {
+    searchInput.addEventListener('input', inputSearch, false)
+  }
+
+  function inputSearch() {
+    var val = _trimSpace(this.value),
+      len = val.length
+
+    if (len) {
+      getDatas(val, 'setDatas')
+    } else {
+      // 清除数据
+      wdList.innerHTML = ''
+      listWrap.style.display = 'none'
+    }
+  }
+
+  function getDatas(value, callbackName) {
+    // var oScript = doc.createElement('script')
+    // oScript.src =
+    //   'https://sp0.baidu.com/5a1Fazu8AA54nxGko9WTAnF6hhy/su?wd=' + value + '&cb=' + callbackName
+
+    // doc.body.appendChild(oScript)
+    // doc.body.removeChild(oScript)
+
+    tools.xhr.ajax({
+      url: 'https://sp0.baidu.com/5a1Fazu8AA54nxGko9WTAnF6hhy/su?wd=' + value,
+      type: 'GET',
+      dataType: 'JSONP',
+      jsonp: 'cb',
+      // jsonpCallback: 'test',
+      success: function (data) {
+        renderList(data)
+      }
+    })
+  }
+
+  function renderList(data) {
+    var list = '',
+      resArr = data.s,
+      len = resArr.length,
+      val = _trimSpace(searchInput.value)
+
+    if (len) {
+      resArr.forEach(function (elem) {
+        list += listTpl.replace(/{{(.*?)}}/gim, function (node, key) {
+          return {
+            wdList: elem,
+            wd: _setWdStyle(val, elem)
+          }[key]
+        })
+      })
+
+      wdList.innerHTML = list
+      listWrap.style.display = 'block'
+    } else {
+      //清除数据
+      wdList.innerHTML = ''
+      listWrap.style.display = 'none'
+    }
+  }
+
+  // window.setDatas = function (data) {
+  //   renderList(data)
+  // }
+
+  function _trimSpace(str) {
+    return str.replace(/\s+/gim, '')
+  }
+
+  function _setWdStyle(value, word) {
+    return '<span class="font-normal">' + value + '</span>' + word.replace(value, '')
+  }
+
+  init()
+})(document)
