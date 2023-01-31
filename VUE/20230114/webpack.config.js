a06fb5cf048c21e2d14377352c42dd24b3205a1c
hongqingbaiji <869227436@qq.com>
2023-01-31 20:14:09 +0800

    0131

---
+++ VUE/20230114/webpack.config.js
@@ -0,0 +1,33 @@
+const { resolve } = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/main.js',
+  output: {
+    path: resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  },
+  externals: {
+    vue: 'Vue'
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        use: [
+          {loader:'vue-loader'}
+        ]
+        // loader: 'vue-loader'
+      }
+    ]
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new HtmlWebpackPlugin({
+      template: resolve(__dirname, 'public/index.html')
+    })
+  ]
+}
